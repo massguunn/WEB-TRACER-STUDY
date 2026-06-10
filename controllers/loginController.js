f78d69f47aad1d58b27af08ab6f6799df42f83a7
massguunn <rohadigunawan0@gmail.com>
2026-06-10 14:03:26 +0800

    memperbaiki

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,10 +1,14 @@
 const db = require("../configs/db");
 const bcrypt = require("bcrypt");
 
+// HALAMAN LOGIN
 exports.index = (req, res) => {
-  res.render("login");
+  res.render("login", {
+    error: null,
+  });
 };
 
+// PROSES LOGIN
 exports.login = (req, res) => {
   const { username, password } = req.body;
 
@@ -12,13 +16,21 @@ exports.login = (req, res) => {
   const queryAdmin = "SELECT * FROM admin WHERE nip = ?";
 
   db.query(queryAdmin, [username], async (err, resultAdmin) => {
-    if (err) throw err;
+    if (err) {
+      console.error(err);
+      return res.render("login", {
+        error: "Terjadi kesalahan pada server",
+      });
+    }
 
     // JIKA ADMIN DITEMUKAN
     if (resultAdmin.length > 0) {
       const admin = resultAdmin[0];
 
-      const cekPassword = await bcrypt.compare(password, admin.password);
+      const cekPassword = await bcrypt.compare(
+        password,
+        admin.password
+      );
 
       if (cekPassword) {
         req.session.user = {
@@ -29,51 +41,78 @@ exports.login = (req, res) => {
 
         return res.redirect("/");
       }
+
+      return res.render("login", {
+        error: "Password admin salah!",
+      });
     }
 
     // CEK MAHASISWA
-    const queryMahasiswa = "SELECT * FROM mahasiswa WHERE nim = ?";
+    const queryMahasiswa =
+      "SELECT * FROM mahasiswa WHERE nim = ?";
 
-    db.query(queryMahasiswa, [username], async (err, resultMahasiswa) => {
-      if (err) throw err;
-
-      if (resultMahasiswa.length > 0) {
-        const mahasiswa = resultMahasiswa[0];
-
-        const cekPassword = await bcrypt.compare(password, mahasiswa.password);
-
-        if (cekPassword) {
-          req.session.user = {
-            id: mahasiswa.id,
-            nama: mahasiswa.nama,
-            role: "mahasiswa",
-          };
-
-          return res.redirect("/");
+    db.query(
+      queryMahasiswa,
+      [username],
+      async (err, resultMahasiswa) => {
+        if (err) {
+          console.error(err);
+          return res.render("login", {
+            error: "Terjadi kesalahan pada server",
+          });
         }
-      }
 
-      res.send("Login gagal");
-    });
+        if (resultMahasiswa.length > 0) {
+          const mahasiswa = resultMahasiswa[0];
+
+          const cekPassword = await bcrypt.compare(
+            password,
+            mahasiswa.password
+          );
+
+          if (cekPassword) {
+            req.session.user = {
+              id: mahasiswa.id,
+              nama: mahasiswa.nama,
+              role: "mahasiswa",
+            };
+
+            return res.redirect("/");
+          }
+
+          return res.render("login", {
+            error: "Password mahasiswa salah!",
+          });
+        }
+
+        // USER TIDAK DITEMUKAN
+        return res.render("login", {
+          error: "Username atau password salah!",
+        });
+      }
+    );
   });
 };
 
-exports.dashboard = (req, res) => {
+// DASHBOARD ADMIN
+exports.dashboardAdmin = (req, res) => {
   res.render("admin/dashboard", {
     title: "Dashboard Admin",
     user: req.session.user,
   });
 };
 
-exports.dashboard = (req, res) => {
+// DASHBOARD MAHASISWA
+exports.dashboardMahasiswa = (req, res) => {
   res.render("users/dashboard", {
-    title: "Dashboard Users",
+    title: "Dashboard Mahasiswa",
     user: req.session.user,
   });
 };
 
+// LOGOUT
 exports.logout = (req, res) => {
   req.session.destroy(() => {
-    res.redirect("/");
+    res.redirect("/login");
   });
 };
